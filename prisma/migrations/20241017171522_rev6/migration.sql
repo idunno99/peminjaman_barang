b235a0f6ef08803e79feb6ab6ae7b68f473e7957
Sadam <sadamsh1099@gmail.com>
2024-12-24 11:56:50 +0700

    update

--- prisma/migrations/20241017171522_rev6/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `perangkat` ADD COLUMN `status` BOOLEAN NOT NULL DEFAULT true;
--- prisma/migrations/20241224020715_rev8/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `peminjaman_perangkat` ADD COLUMN `status` ENUM('Diproses', 'Dipinjam', 'Ditolak', 'Selesai') NOT NULL DEFAULT 'Diproses';
-
--- AlterTable
-ALTER TABLE `peminjaman_ruangan` ADD COLUMN `status` ENUM('Diproses', 'Dipinjam', 'Ditolak', 'Selesai') NOT NULL DEFAULT 'Diproses';
